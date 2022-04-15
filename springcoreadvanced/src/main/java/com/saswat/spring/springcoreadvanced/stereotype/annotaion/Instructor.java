d4a41a9d04171923a649a57175238e73b338899d
saswat <saswatkumarpanda515@gmail.com>
2022-04-15 23:45:28 +0530

    added

---
+++ springcoreadvanced/src/main/java/com/saswat/spring/springcoreadvanced/stereotype/annotaion/Instructor.java
@@ -0,0 +1,30 @@
+package com.saswat.spring.springcoreadvanced.stereotype.annotaion;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("inst")
+@Scope("prototype")
+public class Instructor {
+	@Value("10")
+	private int id;
+	@Value("saswat kumar")
+	private String name;
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", name=" + name + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
